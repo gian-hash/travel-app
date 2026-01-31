aa21bb3077f26f329e4c20c7ea52b8df526e0e83
Santagata <gianmarco.santagata@accenture.com>
2026-01-31 21:30:34 +0100

    fix

--- Models/Trip.cs
+++ Models/Trip.cs
@@ -1,8 +1,11 @@
-public class Trip
+namespace TravelAppHybrid.Models
 {
-    public int Id { get; set; }
-    public string Destination { get; set; }
-    public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
-    public string Description { get; set; }
+    public class Trip
+    {
+        public int Id { get; set; }
+        public string? Destination { get; set; } 
+        public string? Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
 }
