c73432b3563a13a55ead3e533053d17b99855833
Santagata <gianmarco.santagata@accenture.com>
2026-02-12 20:03:01 +0100

    auth

--- auth.js
+++ auth.js
@@ -1,7 +1,11 @@
 function login() {
     const pwd = document.getElementById("pwd").value;
+
     if (pwd === "viaggio2025") {
-        window.location.href = "home.html";
+        document.body.classList.add("fade-out");
+        setTimeout(() => {
+            window.location.href = "home.html";
+        }, 650);
     } else {
         document.getElementById("error").textContent = "Password errata";
     }
