082a8497f067f89cd701063edfeab9e47861aab7
Santagata <gianmarco.santagata@accenture.com>
2026-02-15 19:39:36 +0100

    london

--- auth.js
+++ auth.js
@@ -2,6 +2,7 @@ function login() {
     const pwd = document.getElementById("pwd").value;
 
     if (pwd === "viaggio2026") {
+        localStorage.setItem("auth", "true");
         document.body.classList.add("fade-out");
         setTimeout(() => {
             window.location.href = "home.html";
